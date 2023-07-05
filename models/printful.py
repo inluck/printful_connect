021ab4cc586516ca8a9d7dfa830f850f4555e403
Jon Mitchell <jon@altinc.ca>
2023-07-05 17:34:47 -0230

    Update printful.py

--- models/printful.py
+++ models/printful.py
@@ -370,7 +370,7 @@ class PrintfulPrintful(models.Model):
             }).id
 
     @sleep_and_retry
-    @limits(calls=60, period=60)
+    @limits(calls=30, period=60)
     def _make_api_request(self, url, headers):
         response = requests.get(url, headers=headers)
         if response.status_code == 429:
