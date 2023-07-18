f2b51c98f5cd8bc58b0d6cb88537fe23ff9018b2
Jon Mitchell <jon@altinc.ca>
2023-07-18 20:44:03 -0230

    Update printful_product.py

--- models/printful_product.py
+++ models/printful_product.py
@@ -29,7 +29,8 @@ class ProductProduct(models.Model):
     printful_variant_id = fields.Char()
     printful_product = fields.Many2one(comodel_name='product.template')
     printful_shipping = fields.Char(string='Estimated Delivery')
-
+    printful_sizeguide = fields.Char(string='Size Guide')
+    
     def action_set_printful_data(self):
         for rec in self:
             rem = False
